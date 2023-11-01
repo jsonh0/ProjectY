eeaa2da850215eb06155e4e4d7fcd3317e364825
jason ho <43012595+jsonh0@users.noreply.github.com>
2023-11-01 16:53:56 +0000

    added carrierwave and cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
+require "carrierwave"
+require "carrierwave/orm/activerecord"
 
 Rails.application.configure do
   # Allow server to be hosted on any URL
