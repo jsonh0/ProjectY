1deda82ffc9b41136560afadb2ca5c7412078bbb
jason ho <43012595+jsonh0@users.noreply.github.com>
2023-11-08 20:10:21 +0000

    Added conditions for navbar items, added form for new account.

--- app/models/account.rb
+++ app/models/account.rb
@@ -14,4 +14,8 @@ class Account < ApplicationRecord
   has_many :accesses
   has_many :users, through: :accesses
   has_many :foriegn_nationals
+  accepts_nested_attributes_for :accesses
+  def account_params
+    params.require(:account).permit(:name, :notes, access_attributes: [:role])
+  end
 end
