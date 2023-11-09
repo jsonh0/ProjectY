e20f67999cca979dba539982c470ac0bcb53bb40
jason ho <43012595+jsonh0@users.noreply.github.com>
2023-11-09 16:29:52 +0000

    Permited username and admin? in devise user create/update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :admin])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :admin])
+  end
 
   
 end
