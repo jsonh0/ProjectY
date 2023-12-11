5be7dd3d5959538eed6b66a2baa8c7221c87e9cc
jason ho <43012595+jsonh0@users.noreply.github.com>
2023-12-11 18:34:08 +0000

    changed back to application controler

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 # app/controllers/users_controller.rb
-class UserController < Devise::RegistrationsController
+class UserController < ApplicationController
   before_action :authenticate_user! # Add this if you want to ensure users are authenticated for these actions
 
   def index
